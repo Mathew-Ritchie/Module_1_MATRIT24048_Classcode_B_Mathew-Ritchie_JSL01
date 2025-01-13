296348aa081c181dd46cb0b1b24b43ccd9bb6482
Mathew Ritchie <ritchmat89@gmail.com>
2025-01-13 10:34:43 +0200

    added some comments to JS script

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 function validateSyntax() {
   let input = document.getElementById("petInput").value;
 
-  let pattern = /^pet_+[\d\w]+/;
+  let pattern = /^pet_+[\d\w]+/; //Regex pattern perameters, input must start with pet_ and then followed by a name consisting of either letters, numbers, or a combination.
 
+  //if else statement to confirm if input value follows Regex pattern requirments or not.
   if (pattern.test(input)) {
     document.getElementById("result").innerText = "Valid Syntax";
   } else {
     document.getElementById("result").innerText = "Invalid Syntax";
   }
 }
-
-//let result = ''; // Placeholder for validation result
-
-// TODO: Write your validation logic here
-// Check if input starts with 'pet_' and followed by alphanumeric characters
-
-//document.getElementById("result").innerText = result;
